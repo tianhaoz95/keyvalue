f6bd1854ce3b6277b6030417a29fd8824f10e2f0
Tianhao Zhou <tianhaozhou95@icloud.com>
2026-02-27 20:17:54 -0800

    u

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CPA Engagement App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
